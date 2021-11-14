1c52c0c572aec7c6eeaa5c102559ba1e73d36d23
Alexander Bondarenko <sascha.bondarenko@gmail.com>
2021-11-14 15:42:11 +0100

    simplest implementation

---
+++ queue/queue.py
@@ -0,0 +1,19 @@
+class Queue:
+    def __init__(self):
+        self.arr = []
+
+    def enqueue(self, item):
+        self.arr.append(item)
+
+    def dequeue(self):
+        # выдача из головы
+        if self.size() == 0:
+            return 0
+        return self.arr.pop(0)
+
+    def size(self):
+        return len(self.arr)
+
+    def _rotate(self, n):
+        l = self.arr[n:] + self.arr[:n]
+        self.arr = l
