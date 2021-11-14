1cf52b308e6c735991336a4fc6db976895974d0a
Alexander Bondarenko <sascha.bondarenko@gmail.com>
2021-11-14 15:47:55 +0100

    to update raw file

---
+++ queue/queue_simple.py
@@ -0,0 +1,18 @@
+class Queue:
+    def __init__(self):
+        self.arr = []
+
+    def enqueue(self, item):
+        self.arr.append(item)
+
+    def dequeue(self):
+        if self.size() == 0:
+            return 0
+        return self.arr.pop(0)
+
+    def size(self):
+        return len(self.arr)
+
+    def _rotate(self, n):
+        l = self.arr[n:] + self.arr[:n]
+        self.arr = l
