8b3ab1a228fbece13a996aaba50b642414604dc7
Alexander Bondarenko <sascha.bondarenko@gmail.com>
2021-11-18 17:07:15 +0100

    intermediate

--- binary_search_tree/bst.py
+++ binary_search_tree/bst.py
@@ -11,12 +11,10 @@ class BSTNode:
 class BSTFind:
 
     def __init__(self):
-        self.Node = None  # None если
-        # в дереве вообще нету узлов
+        self.Node = None
 
-        self.NodeHasKey: bool = False  # True если узел найден
-        self.ToLeft: bool = False  # True, если родительскому узлу надо
-        # добавить новый узел левым потомком
+        self.NodeHasKey: bool = False
+        self.ToLeft: bool = False
 
 
 class BST:
@@ -91,9 +89,81 @@ class BST:
                 else:
                     current_node = current_node.LeftChild
 
+    def _remove_if_leaf(self, node: BSTNode) -> bool:
+        is_node_a_leaf = node.LeftChild is None and node.RightChild is None
+        if is_node_a_leaf:
+            is_root_node = node is self.Root
+            if is_root_node:
+                self.Root = None
+                return True
+            parent_node: BSTNode = node.Parent
+            if parent_node.LeftChild is node:
+                parent_node.LeftChild = None
+            if parent_node.RightChild is node:
+                parent_node.RightChild = None
+            return True
+        else:
+            return False
+
+    def _remove_if_no_left_child(self, node: BSTNode) -> bool:
+        if node.LeftChild is None:
+            if node is self.Root:
+                self.Root = node.RightChild
+                return True
+            else:
+                p: BSTNode = node.Parent
+                c: BSTNode = node.RightChild
+                c.Parent = p
+                if node is p.LeftChild:
+                    p.LeftChild = c
+                else:
+                    p.RightChild = c
+            return True
+        return False
+
+    def _remove_if_no_right_child(self, node: BSTNode) -> bool:
+        if node.RightChild is None:
+            if node is self.Root:
+                self.Root = node.LeftChild
+                return True
+            else:
+                p: BSTNode = node.Parent
+                c: BSTNode = node.LeftChild
+                c.Parent = p
+                if node is p.LeftChild:
+                    p.LeftChild = c
+                else:
+                    p.RightChild = c
+                return True
+        return False
+
     def DeleteNodeByKey(self, key) -> bool:
-        # удаляем узел по ключу
-        return False  # если узел не найден
+        f = self.FindNodeByKey(key)
+        is_key_not_found = not f.NodeHasKey
+        if is_key_not_found:
+            return False
+        node: BSTNode = f.Node
+        if self._remove_if_leaf(node):
+            return True
+        if self._remove_if_no_left_child(node):
+            return True
+        if self._remove_if_no_right_child(node):
+            return True
+        is_node_a_head = self.Root == node
+        left_child: BSTNode = node.LeftChild
+        right_child: BSTNode = node.RightChild
+        if is_node_a_head:
+            self.Root = right_child
+        min_max_node = self.FinMinMax(FromNode=right_child, FindMax=False)
+        is_mnn_leaf = min_max_node.LeftChild is None and min_max_node.RightChild is None
+        if is_mnn_leaf:
+            min_max_node.LeftChild = left_child
+            left_child.Parent = min_max_node
+            return True
+        else:
+            assert min_max_node.LeftChild is None
+            #
+            mmn_right_child = min_max_node.RightChild
 
     def Count(self) -> int:
         if self.Root is None:
